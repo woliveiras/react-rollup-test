849e5b648de51a40d41b15db04362bd2a718ee6a
woliveiras <w.oliveira542@gmail.com>
2020-10-29 18:33:27 -0300

    add styled component

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,9 @@
 import babel from 'rollup-plugin-babel';
 import commonjs from '@rollup/plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import postcss from 'rollup-plugin-postcss';
 import resolve from '@rollup/plugin-node-resolve';
-import image from '@rollup/plugin-image'
+import image from '@rollup/plugin-image';
+import url from 'rollup-plugin-url';
 import visualizer from 'rollup-plugin-visualizer';
 import pkg from './package.json';
 
@@ -21,7 +21,7 @@ export default {
   ],
   plugins: [
     external(),
-    postcss(),
+    url(),
     babel({
       exclude: 'node_modules/**'
     }),
@@ -29,5 +29,6 @@ export default {
     commonjs(),
     image(),
     visualizer()
-  ]
+  ],
+  external: ['styled-components']
 };
